444c8e8f99e9fd690ba3a317df1328aa2da48c22
Raphael Marangoni <raphael.marangoni@hipe.cc>
2023-11-24 19:42:08 -0300

    ajustes

--- instagram/routes.py
+++ instagram/routes.py
@@ -59,84 +59,82 @@ def index():
 # Página inicial
 @app.route('/machine', methods=['POST'])
 def machine():
-    
+     # Recebe dados do JSON
     data = request.json
-   
-     # Initialize classifiers
-    knn_classifier = KNeighborsClassifier()
-    svm_classifier = SVC()
-    mlp_classifier = MLPClassifier()
-    dt_classifier = DecisionTreeClassifier()
-    rf_classifier = RandomForestClassifier()
 
-    # Train the models
-    knn_classifier.fit(X_train, y_train)
-    svm_classifier.fit(X_train, y_train)
-    mlp_classifier.fit(X_train, y_train)
-    dt_classifier.fit(X_train, y_train)
-    rf_classifier.fit(X_train, y_train)
-
-    # Make predictions
-    y_pred_knn = knn_classifier.predict(X_test)
-    y_pred_svm = svm_classifier.predict(X_test)
-    y_pred_mlp = mlp_classifier.predict(X_test)
-    y_pred_dt = dt_classifier.predict(X_test)
-    y_pred_rf = rf_classifier.predict(X_test)
-
-      # Matriz de Confusão
-    cm_knn = confusion_matrix(y_test, y_pred_knn).tolist()
-    cm_svm = confusion_matrix(y_test, y_pred_svm).tolist()
-    cm_mlp = confusion_matrix(y_test, y_pred_mlp).tolist()
-    cm_dt = confusion_matrix(y_test, y_pred_dt).tolist()
-    cm_rf = confusion_matrix(y_test, y_pred_rf).tolist()
-
-   # Calcular Acurácia e F1-Score
-    accuracy_knn = accuracy_score(y_test, y_pred_knn)
-    f1_score_knn = f1_score(y_test, y_pred_knn, average='macro')
-
-    accuracy_svm = accuracy_score(y_test, y_pred_svm)
-    f1_score_svm = f1_score(y_test, y_pred_svm, average='macro')
-
-    accuracy_mlp = accuracy_score(y_test, y_pred_mlp)
-    f1_score_mlp = f1_score(y_test, y_pred_mlp, average='macro')
-
-    accuracy_dt = accuracy_score(y_test, y_pred_dt)
-    f1_score_dt = f1_score(y_test, y_pred_dt, average='macro')
-
-    accuracy_rf = accuracy_score(y_test, y_pred_rf)
-    f1_score_rf = f1_score(y_test, y_pred_rf, average='macro')
-
+    # Verifica o tipo de classificador e ajusta os parâmetros correspondentes
     if data['type'] == 'knn':
+        knn_classifier = KNeighborsClassifier(n_neighbors=int(data['parameters']['n_neighbors']), weights=data['parameters']['weights'], p=int(data['parameters']['p']))
+        knn_classifier.fit(X_train, y_train)
+        y_pred_knn = knn_classifier.predict(X_test)
+        cm_knn = confusion_matrix(y_test, y_pred_knn).tolist()
+        accuracy_knn = accuracy_score(y_test, y_pred_knn)
+        f1_score_knn = f1_score(y_test, y_pred_knn, average='macro')
+
         return jsonify({
             'matriz': plot_confusion_matrix(cm_knn),
             'accuracy': accuracy_knn,
             'f1_score': f1_score_knn
         })
 
-    if data['type'] == 'svm':
+    elif data['type'] == 'svm':
+                 # Converte o parâmetro 'C' para um número de ponto flutuante
+        C = float(data['parameters'].get('C', 1.0))
+
+        svm_classifier = SVC(C=C, kernel=data['parameters']['kernel'])
+        svm_classifier.fit(X_train, y_train)
+        y_pred_svm = svm_classifier.predict(X_test)
+        cm_svm = confusion_matrix(y_test, y_pred_svm).tolist()
+        accuracy_svm = accuracy_score(y_test, y_pred_svm)
+        f1_score_svm = f1_score(y_test, y_pred_svm, average='macro')
+
         return jsonify({
             'matriz': plot_confusion_matrix(cm_svm),
             'accuracy': accuracy_svm,
             'f1_score': f1_score_svm
         })
 
-    if data['type'] == 'mlp':
+    elif data['type'] == 'mlp':
+        mlp_classifier = MLPClassifier(hidden_layer_sizes=(int(data['parameters']['hidden_layer_sizes'])), max_iter=int(data['parameters']['max_iter']))
+        mlp_classifier.fit(X_train, y_train)
+        y_pred_mlp = mlp_classifier.predict(X_test)
+        cm_mlp = confusion_matrix(y_test, y_pred_mlp).tolist()
+        accuracy_mlp = accuracy_score(y_test, y_pred_mlp)
+        f1_score_mlp = f1_score(y_test, y_pred_mlp, average='macro')
+
         return jsonify({
             'matriz': plot_confusion_matrix(cm_mlp),
             'accuracy': accuracy_mlp,
             'f1_score': f1_score_mlp
         })
 
-    if data['type'] == 'dt':
+    elif data['type'] == 'dt':
+        dt_classifier = DecisionTreeClassifier(max_depth=int(data['parameters']['max_depth']))
+        dt_classifier.fit(X_train, y_train)
+        y_pred_dt = dt_classifier.predict(X_test)
+        cm_dt = confusion_matrix(y_test, y_pred_dt).tolist()
+        accuracy_dt = accuracy_score(y_test, y_pred_dt)
+        f1_score_dt = f1_score(y_test, y_pred_dt, average='macro')
+
         return jsonify({
             'matriz': plot_confusion_matrix(cm_dt),
             'accuracy': accuracy_dt,
             'f1_score': f1_score_dt
         })
 
-    if data['type'] == 'rf':
+    elif data['type'] == 'rf':
+        rf_classifier = RandomForestClassifier(n_estimators=int(data['parameters']['n_estimators']), max_depth=int(data['parameters']['max_depth']))
+        rf_classifier.fit(X_train, y_train)
+        y_pred_rf = rf_classifier.predict(X_test)
+        cm_rf = confusion_matrix(y_test, y_pred_rf).tolist()
+        accuracy_rf = accuracy_score(y_test, y_pred_rf)
+        f1_score_rf = f1_score(y_test, y_pred_rf, average='macro') 
+
         return jsonify({
             'matriz': plot_confusion_matrix(cm_rf),
             'accuracy': accuracy_rf,
             'f1_score': f1_score_rf
         })
+   
+
+       
--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-"""
-pip install..
-flask
-flask_login
-flask_wtf
-email_validator
-flask_bcrypt
-flask_sqlalchemy
-Flask-Bootstrap4
-
-"""
+    
 from instagram import app
 
 if __name__ == '__main__':
